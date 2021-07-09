6a191cd0affaaa250d79cc4b757729ffd3198440
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2021-07-09 13:55:08 +0200

    weave->mdweave

--- R/mdweave.R
+++ R/mdweave.R
@@ -1,7 +1,7 @@
 
 #' @export
 #' 
-weave <- function(fn, ofn = paste0(fn, ".md")) {
+mdweave <- function(fn, ofn = paste0(fn, ".md")) {
   script <- system.file("scripts/filter.R", package = "simplermarkdown")
   cmd <- sprintf('pandoc -s "%s" --filter "%s" -o "%s"', 
     fn, script, ofn)
