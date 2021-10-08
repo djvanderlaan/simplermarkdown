9e6e12e5a17aa12f7c676e3076c8563699595379
djvanderlaan <jan@knowledge-arcana.nl>
2021-10-08 14:33:02 +0200

    Only check output of mdweave when correct version of pandoc

--- tests/test_mdweave.R
+++ tests/test_mdweave.R
@@ -1,6 +1,15 @@
 # mdweave a couple of documents and check if the output is the
 # same as the corresponding reference documents
 
+
+# We only want to check if the output of mdweave matches the reference if the
+# pandoc version matches that which generated the reference. Otherwise there
+# is a large chance that the output does not match
+pandoc_version <- system("pandoc --version", intern = TRUE)
+correct_pandoc_version <- c("pandoc 2.5",
+  "Compiled with pandoc-types 1.17.5.4, texmath 0.11.2.2, skylighting 0.7.7")
+is_correct_pandoc_version <- all(pandoc_version[1:2] == correct_pandoc_version)
+
 library(tinymarkdown)
 
 # Create tempdir
@@ -22,12 +31,20 @@ message("Weave file")
 fn <- system.file(file.path("examples", md), package = "tinymarkdown")
 mdweave(fn)
 
-message("Compare to reference")
-lines <- readLines(md)
-fn_ref <- system.file(file.path("examples_output", md), 
-  package = "tinymarkdown")
-lines_ref <- readLines(fn_ref)
-stopifnot(isTRUE(all.equal(lines, lines_ref)))
+message("Check if result generated")
+stopifnot(file.exists(md))
+
+if (is_correct_pandoc_version) {
+  message("Compare to reference")
+  lines <- readLines(md)
+  fn_ref <- system.file(file.path("examples_output", md), 
+    package = "tinymarkdown")
+  lines_ref <- readLines(fn_ref)
+  print(all.equal(lines, lines_ref))
+  system(paste0("diff ", md, " ", fn_ref))
+  writeLines(lines)
+  stopifnot(isTRUE(all.equal(lines, lines_ref)))
+}
 
 message("Check images")
 # We don't check if the images are exactly the same; this probably 
@@ -35,7 +52,7 @@ message("Check images")
 stopifnot(file.exists("figures/iris.png"))
 
 message("Cleanup")
-unlink(md)
+#unlink(md)
 unlink("figures")
 
 # =============================================================================
@@ -46,12 +63,19 @@ message("Weave file")
 fn <- system.file(file.path("examples", md), package = "tinymarkdown")
 mdweave(fn)
 
-message("Compare to reference")
-lines <- readLines(md)
-fn_ref <- system.file(file.path("examples_output", md), 
-  package = "tinymarkdown")
-lines_ref <- readLines(fn_ref)
-stopifnot(isTRUE(all.equal(lines, lines_ref)))
+message("Check if result generated")
+stopifnot(file.exists(md))
+
+if (is_correct_pandoc_version) {
+  message("Compare to reference")
+  lines <- readLines(md)
+  fn_ref <- system.file(file.path("examples_output", md), 
+    package = "tinymarkdown")
+  lines_ref <- readLines(fn_ref)
+  print(all.equal(lines, lines_ref))
+  system(paste0("diff ", md, " ", fn_ref))
+  stopifnot(isTRUE(all.equal(lines, lines_ref)))
+}
 
 message("Check images")
 # We don't check if the images are exactly the same; this probably 
